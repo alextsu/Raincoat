9e6ec4e95045e7aca5598287ad3e519589336e19
alextsu <alextsu@gmail.com>
2015-09-29 23:52:03 -0400

    Housekeeping.

--- Raincoat/Raincoat/ViewController.swift
+++ Raincoat/Raincoat/ViewController.swift
@@ -24,8 +24,6 @@ class ViewController: UIViewController, LoadWeatherDataDelegate {
     @IBOutlet weak var highTempLabel: UILabel!
     @IBOutlet weak var lowTempLabel: UILabel!
     
-    var currentlyInSettingsMenu : Bool = false
-    
     var coreLocation : CoreLocation?
     var todaysConditions : TodaysConditions!
     var hourlyConditions : [HourlyCondition]?
@@ -114,7 +112,8 @@ class ViewController: UIViewController, LoadWeatherDataDelegate {
     
     @IBAction func settings(sender: AnyObject) {
         
-        self.currentlyInSettingsMenu = true
+        
+        //self.currentlyInSettingsMenu = true
         
         //set background dark
         
@@ -183,8 +182,8 @@ class ViewController: UIViewController, LoadWeatherDataDelegate {
         UIView.animateWithDuration(1.5, animations: {
             self.settingsView.center.y += (500 + self.view.frame.height/2 - self.settingsView.frame.height/2)
             self.settingsView.alpha = 1.0
-        }, completion: { finished in
-            println("Moved!")
+            }, completion: { finished in
+                println("Moved!")
         })
         
     }
@@ -194,10 +193,10 @@ class ViewController: UIViewController, LoadWeatherDataDelegate {
         UIView.animateWithDuration(0.7, animations: {
             self.settingsView.center.y -= (500 + self.view.frame.height/2 - self.settingsView.frame.height/2)
             self.settingsView.alpha = 0.0
-        }, completion: { finished in
-            println("Moved!")
-            UIView.animateWithDuration(1, animations: {self.tempBackgroundScreen.alpha = 0.0}, completion: { finished in })
-            self.navigationController?.navigationBar.alpha = 1
+            }, completion: { finished in
+                println("Moved!")
+                UIView.animateWithDuration(1, animations: {self.tempBackgroundScreen.alpha = 0.0}, completion: { finished in })
+                self.navigationController?.navigationBar.alpha = 1
         })
     }
     
@@ -207,28 +206,33 @@ class ViewController: UIViewController, LoadWeatherDataDelegate {
         NSUserDefaults.standardUserDefaults().setObject(timePicker.date, forKey: "AlarmTime")
         //NSLog("Time is: %@", timePicker.date.description)
         
+        UIApplication.sharedApplication().cancelAllLocalNotifications()
+        
         self.triggerAlarm(timePicker.date, alarmSetting: self.alarmSetting.selectedSegmentIndex)
         
         
         UIView.animateWithDuration(0.7, animations: {
             self.settingsView.center.y -= (500 + self.view.frame.height/2 - self.settingsView.frame.height/2)
             self.settingsView.alpha = 0.0
-        }, completion: { finished in
-            println("Moved!")
-            UIView.animateWithDuration(1, animations: {self.tempBackgroundScreen.alpha = 0.0}, completion: { finished in
-            
-                dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(0.5 * Double(NSEC_PER_SEC))), dispatch_get_main_queue()) { () -> Void in
-
-                    let alert = UIAlertView()
-                    alert.title = "Alarm Saved"
-                    alert.message = "Please keep Raincoat running in the background if you wish to receive subsequent rain alarms."
-                    alert.addButtonWithTitle("OK")
-                    alert.show()
-                }
-
-            
-            })
-            self.navigationController?.navigationBar.alpha = 1
+            }, completion: { finished in
+                println("Moved!")
+                UIView.animateWithDuration(1, animations: {self.tempBackgroundScreen.alpha = 0.0}, completion: { finished in
+                    
+                    if self.alarmSetting.selectedSegmentIndex == 0 {
+                        
+                        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(0.1 * Double(NSEC_PER_SEC))), dispatch_get_main_queue()) { () -> Void in
+                            
+                            let alert = UIAlertView()
+                            alert.title = "Alarm Saved"
+                            alert.message = "Please keep Raincoat running in the background if you wish to receive subsequent rain alarms."
+                            alert.addButtonWithTitle("OK")
+                            alert.show()
+                        }
+                        
+                    }
+                    
+                })
+                self.navigationController?.navigationBar.alpha = 1
         })
         
         
@@ -276,7 +280,7 @@ class ViewController: UIViewController, LoadWeatherDataDelegate {
             UIApplication.sharedApplication().scheduleLocalNotification(notification)
             
         }
-
+        
     }
     
     
@@ -299,19 +303,27 @@ class ViewController: UIViewController, LoadWeatherDataDelegate {
     
     func cloudMoveLeft(cloud : UIView) {
         self.currentWeather = "rainy"
-        UIView.animateWithDuration(5.0,
+        UIView.animateWithDuration(3.0,
             delay: 0.0,
-            options: nil,
-            animations: {cloud.center.x += 11},
-            completion: {finished in self.cloudMoveRight(cloud)})
+            options: UIViewAnimationOptions.CurveEaseInOut,
+            animations: {cloud.center.x += 20},
+            completion: {finished in
+                if finished {
+                    self.cloudMoveRight(cloud)
+                }
+        })
     }
     
     func cloudMoveRight(cloud: UIView) {
-        UIView.animateWithDuration(5.0,
+        UIView.animateWithDuration(3.0,
             delay: 0.0,
-            options: nil,
-            animations: {cloud.center.x -=  11},
-            completion: {finished in self.cloudMoveLeft(cloud)})
+            options: UIViewAnimationOptions.CurveEaseInOut,
+            animations: {cloud.center.x -=  20},
+            completion: {finished in
+                if finished {
+                    self.cloudMoveLeft(cloud)
+                }
+        })
     }
     
     func willRainToday(conditions: [HourlyCondition]) -> Bool {
@@ -326,73 +338,72 @@ class ViewController: UIViewController, LoadWeatherDataDelegate {
     
     func loadData() {
         
-        if currentlyInSettingsMenu == false {
+        
+        self.currentTemp = weatherJSONRequester.getCurrentTemp()!
+        self.todaysConditions = weatherJSONRequester.getTodaysConditions()
+        self.hourlyConditions = weatherJSONRequester.getHourlyConditions()
+        
+        //Should wear label
+        var city = NSUserDefaults.standardUserDefaults().objectForKey("city") as! String
+        shouldWearLabel.text = "Will it rain in\n\(city) today?"
+        
+        //Set high low
+        highTempLabel.text = String(todaysConditions.high) + "\u{00B0}"
+        lowTempLabel.text = String(todaysConditions.low) + "\u{00B0}"
+        
+        //Details label
+        weatherDetailsLabel.text = "Currently " + String(self.currentTemp) + "F. " + self.todaysConditions.forecastText
+        
+        //Bring it label
+        if willRainToday(self.hourlyConditions!) == true {
+            bringItLabel.text = "Yup. Grab a raincoat!"
             
-            self.currentTemp = weatherJSONRequester.getCurrentTemp()!
-            self.todaysConditions = weatherJSONRequester.getTodaysConditions()
-            self.hourlyConditions = weatherJSONRequester.getHourlyConditions()
+            self.view.backgroundColor = raincoatNavy
+            self.navigationController?.navigationBar.tintColor = raincoatYellow
+            self.highTempLabel.textColor = raincoatYellow
+            self.lowTempLabel.textColor = raincoatYellow
+            self.weatherDetailsLabel.textColor = raincoatYellow
+            self.bringItLabel.textColor = raincoatYellow
             
-            //Should wear label
-            var city = NSUserDefaults.standardUserDefaults().objectForKey("city") as! String
-            shouldWearLabel.text = "Will it rain in\n\(city) today?"
+            self.sunback.alpha = 0
+            self.sunfront.alpha = 0
             
-            //Set high low
-            highTempLabel.text = String(todaysConditions.high) + "\u{00B0}"
-            lowTempLabel.text = String(todaysConditions.low) + "\u{00B0}"
+            self.view.bringSubviewToFront(cloudFront)
+            self.view.bringSubviewToFront(tempBackgroundScreen)
+            self.view.bringSubviewToFront(raincoatLogo)
             
-            //Details label
-            weatherDetailsLabel.text = "Currently " + String(self.currentTemp) + "F. " + self.todaysConditions.forecastText
-            
-            //Bring it label
-            if willRainToday(self.hourlyConditions!) == true {
-                bringItLabel.text = "Yup. Grab a raincoat!"
-                
-                self.view.backgroundColor = raincoatNavy
-                self.navigationController?.navigationBar.tintColor = raincoatYellow
-                self.highTempLabel.textColor = raincoatYellow
-                self.lowTempLabel.textColor = raincoatYellow
-                self.weatherDetailsLabel.textColor = raincoatYellow
-                self.bringItLabel.textColor = raincoatYellow
-                
-                self.sunback.alpha = 0
-                self.sunfront.alpha = 0
-                
-                self.view.bringSubviewToFront(cloudFront)
-                self.view.bringSubviewToFront(tempBackgroundScreen)
-                self.view.bringSubviewToFront(raincoatLogo)
-                
-                //check if the clouds are already moving. If not, begin animation
-                if(currentWeather != "rainy") {
-                    cloudMoveLeft(cloudFront)
-                    cloudMoveRight(cloudBack)
-                }
+            //check if the clouds are already moving. If not, begin animation
+            if(currentWeather != "rainy") {
+                //cloudMoveLeft(cloudFront)
+                //cloudMoveRight(cloudBack)
             }
-            else {
-                bringItLabel.text = "Nope. We're rain-free!"
-                
-                self.cloudBack.alpha = 0.0
-                self.cloudFront.alpha = 0.0
-                
-                self.navigationItem.rightBarButtonItem?.tintColor = raincoatNavy
-                self.navigationItem.leftBarButtonItem?.tintColor = raincoatNavy
-                
-                //check if the sun is already moving. If not, begin animation
-                if(currentWeather != "sunny") {
-                    sunRotateOnce()
-                }
-            }
-
+        }
+        else {
+            bringItLabel.text = "Nope. We're rain-free!"
             
-            self.navigationItem.rightBarButtonItem!.enabled = true
-            UIView.animateWithDuration(0.9, animations: {
-                self.raincoatLogo.alpha = 0.0
+            self.cloudBack.alpha = 0.0
+            self.cloudFront.alpha = 0.0
+            
+            self.navigationItem.rightBarButtonItem?.tintColor = raincoatNavy
+            self.navigationItem.leftBarButtonItem?.tintColor = raincoatNavy
+            
+            //check if the sun is already moving. If not, begin animation
+            if(currentWeather != "sunny") {
+                sunRotateOnce()
+            }
+        }
+        
+        
+        self.navigationItem.rightBarButtonItem!.enabled = true
+        UIView.animateWithDuration(0.9, animations: {
+            self.raincoatLogo.alpha = 0.0
             }, completion: { finished in
                 UIView.animateWithDuration(0.9, animations: {
                     self.tempBackgroundScreen.alpha = 0.0
-
+                    
                     }, completion: { finished in })
-            })
-        }
+        })
+        
         
         //Welcome Message
         let defaults = NSUserDefaults.standardUserDefaults()
@@ -404,7 +415,7 @@ class ViewController: UIViewController, LoadWeatherDataDelegate {
                 NSLog("First Time")
                 let alert = UIAlertView()
                 alert.title = "Welcome to Raincoat"
-                alert.message = "To get started, click the Settings button to set your Raincoat alarm."
+                alert.message = "To get started, click the Settings button to set your Raincoat alarm. Please note, in order for you to receive daily alarms, Raincoat must be running in the background of your device."
                 alert.addButtonWithTitle("OK")
                 alert.show()
             }
